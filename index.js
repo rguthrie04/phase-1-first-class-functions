ee687224d9138809ccdaa6e8e12bd4b93676d39c
Ryan Guthrie <ryanguthrie@zoho.com>
2021-08-11 14:28:57 +0100

    commit tree 325be3b872a4896c995536c4a723899e7049a069

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(tea) {
+    tea();
+}
+
+
+function returnsANamedFunction() {
+    return aNamedFunction
+}
+function aNamedFunction() {
+    
+}
+function returnsAnAnonymousFunction() {
+    return () => {
+        
+    }
+}
